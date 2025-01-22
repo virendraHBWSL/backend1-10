e95361a1e7a40a727e681a84d018058eaa940fc3
virendra patil <virendra.patil@hbwsl.com>
2025-01-22 17:46:13 +0530

    link11:layout,block,template

--- code/Mageplaza/HelloWorld/Controller/Index/Index.php
+++ code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -1,43 +1,43 @@
-namespace Mageplaza\HelloWorld\Controller\Index;
+
+
+<!-- namespace Mageplaza\HelloWorld\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\ResultInterface;
-use Mageplaza\HelloWorld\Api\PostRepositoryInterface;
-
+use Mageplaza\HelloWorld\Model\PostFactory;
+ 
 class Index implements HttpGetActionInterface
 {
     private PageFactory $pageFactory;
-    private PostRepositoryInterface $postRepository;
+    // private PostFactory $postFactory;
 
     public function __construct(
         PageFactory $pageFactory,
-        PostRepositoryInterface $postRepository
+        PostFactory $postFactory
     ) {
         $this->pageFactory = $pageFactory;
-        $this->postRepository = $postRepository;
+        $this->postFactory = $postFactory;
     }
 
     public function execute(): ResultInterface
     {
-        // Load posts from the PostRepository
-        $posts = $this->postRepository->getList();
+        // Load posts from the PostFactory
+        $post = $this->postFactory->create();
+        $collection = $post->getCollection();
 
         // Prepare the data for output
         $data = [];
-        foreach ($posts as $post) {
-            $data[] = $post->getData();
+        foreach ($collection as $item) {
+            $data[] = $item->getData();
         }
 
-        // Render the page result
+        // Render the page result (you can modify this part for other types of results if needed)
         $pageResult = $this->pageFactory->create();
 
-        // Set the posts data in the block
-        $block = $pageResult->getLayout()->getBlock('content');
-        if ($block) {
-            $block->setData('posts', $data);
-        }
+        // You can pass the $data to the page's layout if needed
+        $pageResult->getLayout()->getBlock('content')->setData('posts', $data);
 
         return $pageResult;
     }
-}
+}   -->
