e4165847cb1cd9c8d65978ce5ad45feab34e67a5
virendra patil <virendra.patil@hbwsl.com>
2025-01-23 17:16:13 +0530

    link 24: graphql

---
+++ code/Mageplaza/HelloWorld/Model/Resolver/CustomData.php
@@ -0,0 +1,17 @@
+<?php
+namespace Vendor\Module\Model\Resolver;
+
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class CustomData implements ResolverInterface
+{
+    public function resolve(\Magento\Framework\GraphQl\Config\Element\Field $field, $context, \Magento\Framework\GraphQl\Schema\Type\ResolveInfo $info, array $value = null, array $args = null)
+    {
+        // Fetch and return custom data
+        return [
+            'id' => 1,
+            'name' => 'Custom Data',
+            // Add more data as needed
+        ];
+    }
+}
